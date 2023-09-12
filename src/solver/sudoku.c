114f2ded54c79b39004de319e5b09c92fa076081
8dcc <8dcc.git@gmail.com>
2023-09-12 22:59:41 +0200

    Improve logic and performance of sudoku.c
    Change comment style, improve comments

--- src/solver/sudoku.c
+++ src/solver/sudoku.c
@@ -1,27 +1,34 @@
 
+#include <stdbool.h>
 #include <stdio.h>
-#include <ctype.h>    // isdigit()
+#include <ctype.h> /* isdigit() */
+
+#include "sudoku.h"
 #include "defines.h"
 #include "misc.h"
 
-void init_arr(int* arr) {
-    for (int y = 0; y < ROWS; y++)
-        for (int x = 0; x < COLS; x++)
-            arr[COLS * y + x] = UNK;
-}
+/*----------------------------------------------------------------------------*/
 
 static inline int is_valid(int c) {
-    // Digit or '?'
+    /* Digit or '?' */
     return isdigit(c) || c == UNK_C;
 }
 
 static inline int c2i(int c) {
-    // Not a char, invalid
+    /* Not a char, invalid */
     if (!is_valid(c))
         die("c2i: tried to convert complex integer (%d) to char.\n", c);
 
-    // Convert char to int or UNK_C ('?') to UNK int (-1)
+    /* Convert char to int or UNK_C ('?') to UNK int (-1) */
     return (c == UNK_C) ? UNK : c - '0';
+}
+
+/*----------------------------------------------------------------------------*/
+
+void init_arr(int* arr) {
+    for (int y = 0; y < ROWS; y++)
+        for (int x = 0; x < COLS; x++)
+            arr[y * COLS + x] = 0;
 }
 
 int read_arr(FILE* fd, int* arr) {
@@ -32,17 +39,13 @@ int read_arr(FILE* fd, int* arr) {
     int x = 0, y = 0;
 
     while ((c = getc(fd)) != EOF && y < ROWS) {
-        // We need the >= instead of just > because arr[COLS] would be out of bounds.
-        // Same with the rows.
         if (x >= COLS) {
             x = 0;
             y++;
         }
 
-        // We use the < to check if we are in bounds, keep in mind COLS is the number
-        // of items and not the last valid idx.
         if (is_valid(c) && x < COLS) {
-            arr[COLS * y + x] = c2i(c);
+            arr[y * COLS + x] = c2i(c);
             x++;
         }
     }
@@ -50,49 +53,46 @@ int read_arr(FILE* fd, int* arr) {
     return 0;
 }
 
-// Will put gray in the number if unk[cur_arr_idx] == 1
-void print_arr(int* arr, int* unk_arr) {
-    int c;
+/* The was_unk array is used to know if each cell was unknown before solving. If
+ * it was, we print it with a different color. It's filled in main() */
+void print_arr(int* arr, bool* was_unk) {
     char* NUM_COL;
 
     for (int y = 0; y < ROWS; y++) {
-        // For showing the main 9 squares (rows)
+        /* For showing the main 9 squares (rows) */
         if (y % 3 == 0)
             printf("%s", BOLD);
 
-        // Border before each row
+        /* Border before each row */
         for (int x = 0; x < COLS; x++) {
-            // For showing the main 9 squares ('+' chars of row separators)
-            // Only leave "---" part white if we are not in a full bold row.
             if (y % 3 != 0 && x % 3 == 0)
                 printf(BOLD "+" SOFT "---");
             else
                 printf("+---");
         }
-        // Last '+' is always bold
+
+        /* Last '+' is always bold */
         printf(BOLD "+" NORM "\n");
 
-        // Reset colors if we just drew a bold one
+        /* Reset colors if we just drew a bold one */
         if (y % 3 == 0)
             printf("%s", NORM);
 
-        // Row itself
+        /* Row itself */
         for (int x = 0; x < COLS; x++) {
             int idx = COLS * y + x;
+            int c   = arr[idx];
 
-            c = arr[idx];
-
-            // If array pos is UNK (-1), change char
+            /* Convert to char */
             if (c == UNK)
-                c = UNK_C;    // '?'
+                c = UNK_C; /* '?' */
             else
-                c += '0';
+                c += '0'; /* 5 -> '5' */
 
-            // If we know the current item should be hidden (is 1 in the array),
-            // change colors.
-            NUM_COL = (unk_arr[idx]) ? NFCOL : FCOL;
+            /* Change color if it was hidden before solving */
+            NUM_COL = was_unk[idx] ? NFCOL : FCOL;
 
-            // For showing the main 9 squares (cols)
+            /* For showing the main 9 squares (cols) */
             if (x % 3 == 0)
                 printf(BOLD "|%s %c " NORM, NUM_COL, c);
             else
@@ -101,71 +101,61 @@ void print_arr(int* arr, int* unk_arr) {
         printf(BOLD "|" NORM "\n");
     }
 
-    // Border after last row (bold too)
+    /* Border after last row */
     printf("%s", BOLD);
     for (int x = 0; x < COLS; x++)
         printf("+---");
     printf("+" NORM "\n");
 }
 
-void print_unk_arr(int* arr) {
-    int empty[ROWS * COLS] = { 0 };
-
-    // Fill the empty array with 1's where there was an UNK
-    for (int y = 0; y < ROWS; y++)
-        for (int x = 0; x < COLS; x++)
-            empty[COLS * y + x] = (arr[COLS * y + x] == UNK);
-
-    print_arr(arr, empty);
-}
-
 static int valid_pos(int* arr, int idx, int num) {
-    // Get x and y pos in the arr from idx
+    /* Get x and y pos in the arr from idx */
     const int yp = idx / COLS;
     const int xp = idx - yp * COLS;
 
-    // Check current row
+    /* Check current row */
     for (int x = 0; x < COLS; x++)
-        if (arr[COLS * yp + x] == num)
+        if (arr[yp * COLS + x] == num)
             return 0;
 
-    // Check current col
+    /* Check current col */
     for (int y = 0; y < ROWS; y++)
-        if (arr[COLS * y + xp] == num)
+        if (arr[y * COLS + xp] == num)
             return 0;
 
-    // Idx of cells corresponding to each box by y and x of the box
+    /* Idx of cells corresponding to each box by Y and X of the box */
     static const int boxes[COLS][ROWS][BOXSZ] = {
-        // First row of boxes (y1)
         {
-          { 0, 1, 2, 9, 10, 11, 18, 19, 20 },     // y0x0
-          { 3, 4, 5, 12, 13, 14, 21, 22, 23 },    // y0x1
-          { 6, 7, 8, 15, 16, 17, 24, 25, 26 }     // y0x2
+          /* Top row of boxes */
+          { 0, 1, 2, 9, 10, 11, 18, 19, 20 },  /* y0, x0 */
+          { 3, 4, 5, 12, 13, 14, 21, 22, 23 }, /* y0, x1 */
+          { 6, 7, 8, 15, 16, 17, 24, 25, 26 }  /* y0, x2 */
         },
-        // Middle row of boxes (y2)
         {
-          { 27, 28, 29, 36, 37, 38, 45, 46, 47 },    // y1x0
-          { 30, 31, 32, 39, 40, 41, 48, 49, 50 },    // y1x1
-          { 33, 34, 35, 42, 43, 44, 51, 52, 53 }     // y1x2
+          /* Middle row of boxes */
+          { 27, 28, 29, 36, 37, 38, 45, 46, 47 }, /* y1, x0 */
+          { 30, 31, 32, 39, 40, 41, 48, 49, 50 }, /* y1, x1 */
+          { 33, 34, 35, 42, 43, 44, 51, 52, 53 }  /* y1, x2 */
         },
-        // Bottom row of boxes
         {
-          { 54, 55, 56, 63, 64, 65, 72, 73, 74 },    // y2x0
-          { 57, 58, 59, 66, 67, 68, 75, 76, 77 },    // y2x1
-          { 60, 61, 62, 69, 70, 71, 78, 79, 80 }     // y2x2
-        }
+          /* Bottom row of boxes */
+          { 54, 55, 56, 63, 64, 65, 72, 73, 74 }, /* y2, x0 */
+          { 57, 58, 59, 66, 67, 68, 75, 76, 77 }, /* y2, x1 */
+          { 60, 61, 62, 69, 70, 71, 78, 79, 80 }  /* y2, x2 */
+        },
     };
 
-    // Check current box
-    for (int box_i = 0; box_i < BOXSZ; box_i++)
-        /*
-         * Divide x and y by 3 to get the box position (top-left, bottom-middle,
-         * ...), use them to specify which box we want to check. We are iterating the
-         * items inside that box to check if the number we want to check (num) is any
-         * of the indexes that form that box.
-         */
-        if (arr[boxes[yp / 3][xp / 3][box_i]] == num)
+    /* Get current box idx from YP and XP, then get array of indexes for that */
+    const int box_y            = yp / 3;
+    const int box_x            = xp / 3;
+    const int* cur_box_indexes = &boxes[box_y][box_x][0];
+
+    /* Check if number is in current box by checking each valid index */
+    for (int box_i = 0; box_i < BOXSZ; box_i++) {
+        const int idx_to_check = cur_box_indexes[box_i];
+        if (arr[idx_to_check] == num)
             return 0;
+    }
 
     return 1;
 }
@@ -177,61 +167,54 @@ static int valid_pos(int* arr, int idx, int num) {
  *     ...
  * };
  *
- * Where ? is UNK (-1).
+ * Where ? is stored as UNK (-1).
+ * We will locally store the raw indexes (2d -> 1d) of those UNK values in
+ * another array called unk_indexes.
  */
 void solve(int* arr) {
-    /*
-     * Array that will hold a list of the unknown indexes of arr. Assuming 3 and 5
-     * are known:
-     *   ucells = { 1, 2, 4, 6, 7, ... };
-     */
-    int ucells[ROWS * COLS] = { 0 };
-    int ucells_last         = 0;
+    /* Array that will hold a list of the unknown indexes of arr. */
+    int unk_indexes[ROWS * COLS] = { 0 };
+    int unk_indexes_sz           = 0;
 
-    // Check each cell of arr. If the current cell is unknown, save idx in the ucells
-    // array. ucells_last will be the last valid idx + 1
+    /* Store unknown cells of arr in unk_indexes. Also store array size. */
     for (int y = 0; y < ROWS; y++)
         for (int x = 0; x < COLS; x++)
-            if (arr[COLS * y + x] == UNK)
-                ucells[ucells_last++] = COLS * y + x;
+            if (arr[y * COLS + x] == UNK)
+                unk_indexes[unk_indexes_sz++] = y * COLS + x;
 
-    // Used to check if we found a valid number when checking the unknown cells. If
-    // we check all numbers and none is valid, we will go back to the previous
-    // unknown cell and increase the value by 1.
-    int found_valid = 0;
+    /* Iterate unkown cells in unk_indexes */
+    for (int i = 0; i < unk_indexes_sz; i++) {
+        /* Get the current index we want to check from unk_indexes array */
+        int cur_idx = unk_indexes[i];
 
-    // Each unknown cell idx
-    for (int i = 0; i < ucells_last; i++) {
-        found_valid = 0;
+        /* Will store the first valid number we can use on the current cell */
+        int valid = 0;
 
-        // Try numbers from 1 to 9. If the current unknown cell has been written
-        // before (has a val), start from there.
-        int first_check = (arr[ucells[i]] == UNK) ? 1 : arr[ucells[i]];
-        for (int n = first_check; n <= 9; n++) {
-            if (valid_pos(arr, ucells[i], n)) {
-                found_valid = n;
+        /* Try 1 to 9 for current unknown cell. If the current cell we are
+         * checking has a value, use that one. Start from 1 otherwise. */
+        int starting_value = (arr[cur_idx] == UNK) ? 1 : arr[cur_idx];
+        for (int num = starting_value; num <= 9; num++) {
+            if (valid_pos(arr, cur_idx, num)) {
+                valid = num;
                 break;
             }
         }
 
-        // A cell can't be 0
-        if (found_valid > 0) {
-            arr[ucells[i]] = found_valid;
-        } else {
-            // If we are on the first cell and we can't find a valid num, sudoku is
-            // unsolvable.
-            if (i < 1)
-                die("solve: can't find a valid number for the first unknown cell\n",
-                    ucells[0]);
-
-            // Reset the cell we could not find to -1
-            arr[ucells[i]] = UNK;
-
-            // If no number is valid in the current cell, go back to the previous one
-            // and start from there. We do -2 because it's gonna do i++ after
-            // continue.
-            i -= 2;
+        /* If we found a valid number, set it and check the next unknown */
+        if (valid != 0) {
+            arr[cur_idx] = valid;
+            continue;
         }
+
+        /* If this is the first unknown index we are checking, and it didn't
+         * have a solution, mark as unsolvable. */
+        if (i <= 0)
+            die("solve: no valid number for the first unknown cell\n");
+
+        /* Not the first unknown index, reset the cell to UNK and go back to the
+         * previous unknown value from the unk_indexes array. We subtract 2
+         * because the for loop will ad 1 */
+        arr[cur_idx] = UNK;
+        i -= 2;
     }
 }
-
--- src/solver/sudoku.h
+++ src/solver/sudoku.h
@@ -1,7 +1,12 @@
 
+#ifndef SUDOKU_H_
+#define SUDOKU_H_ 1
+
+#include <stdbool.h>
+
 void init_arr(int* arr);
 int read_arr(FILE* fd, int* arr);
-void print_arr(int* arr, int* unk_arr);
-void print_unk_arr(int* arr);
+void print_arr(int* arr, bool* empty);
 void solve(int* arr);
 
+#endif /* SUDOKU_H_ */
